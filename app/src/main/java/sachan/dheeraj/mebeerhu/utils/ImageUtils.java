e59de5b4333eb34385173d387148f9d915b60398
dheeraj <dhirajsiitk9@gmail.com>
2015-08-05 13:44:03 +0530

    post

--- app/src/main/java/sachan/dheeraj/mebeerhu/utils/ImageUtils.java
+++ app/src/main/java/sachan/dheeraj/mebeerhu/utils/ImageUtils.java
@@ -4,7 +4,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Hashtable;
 
 /**
